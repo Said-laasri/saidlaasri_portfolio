43bfee294aa8f95ac069f89c513746fb9cf3f868
Said-laasri <saidlaasri3@gmail.com>
2023-04-12 17:38:57 +0000

    Using form component from boostrap

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,10 +1,29 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <Form className="m-5 p-4">
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Your Name:</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Your Email address:</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Message</Form.Label>
+        <Form.Control size="lg" as="textarea" rows={3} />
+      </Form.Group>
+
+      <Button variant="outline-dark" className="mb-4" size="lg" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 };
 
