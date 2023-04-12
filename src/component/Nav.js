0a0b574d83dbb6b40fd91e2f2cb612a277febe9f
Said-laasri <saidlaasri3@gmail.com>
2023-04-12 17:39:36 +0000

    Navbar boostrap component

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,20 +1,27 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const Nav = () => {
+const Navigation = () => {
   return (
-    <nav>
-      <div>logo</div>
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Services</li>
-          <li>Portfolio</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-    </nav>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" posi>
+      <Container>
+        <Navbar.Brand href="#home">Said Laasri</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="home">Home</Nav.Link>
+            <Nav.Link href="about">About</Nav.Link>
+            <Nav.Link href="project">Project</Nav.Link>
+            <Nav.Link href="skills">Skills</Nav.Link>
+            <Nav.Link href="contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Nav;
+export default Navigation;
