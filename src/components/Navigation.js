9c509b0d74dd14ff8c521ec89523641061a8c527
Said-laasri <saidlaasri3@gmail.com>
2023-04-26 16:33:33 +0100

    Add testemony link to the navigation and toggle them button to change app style

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,25 +1,37 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import BUttonTheme from "./UI/ButtonTheme";
 import "bootstrap/dist/css/bootstrap.min.css";
+import classes from "./Navigation.module.css";
 
-const Navigation = () => {
+const Navigation = (props) => {
+  const { onChangeTheme } = props;
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" >
-      <Container>
-        <Navbar.Brand href="#home">Said Laasri</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#project">Project</Nav.Link>
-            <Nav.Link href="#tech">Skills</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    <Navbar
+      className={classes.nav}
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+    >
+      <Navbar.Brand href="#home" className={classes.brand}>
+        Said Laasri
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav" className={classes.links}>
+        <Nav className={classes.navLinks}>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#project">Project</Nav.Link>
+          <Nav.Link href="#tech">Skills</Nav.Link>
+          <Nav.Link href="#testemony">Testimonies</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+          <Nav.Link href="#resume">
+            <BUttonTheme onChangeTheme={onChangeTheme} />
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
