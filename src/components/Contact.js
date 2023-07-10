55000c4cb0cb6ff48ee85be486a70d2a4ca553f0
Said-laasri <saidlaasri3@gmail.com>
2023-07-10 14:45:48 +0100

    From activation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,4 @@
+import React, { useRef } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
@@ -5,11 +6,36 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Map from "./Map";
 import "./contact.css";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_py0iu92",
+        "template_jus700g",
+        form.current,
+        "NW72zDIhxyX4_mL-U"
+      )
+      .then(
+        () => {
+          alert("Message successfully sent!");
+          window.location.reload(false);
+        },
+        () => {
+          alert("Failed to send the message, please try again");
+        }
+      );
+  };
+
   return (
-    <Container >
-      <h1 className="section text-center mt-4 p-2" data-aos="fade-right">Contact Me</h1>
+    <Container id="contact">
+      <h1 className="section text-center mt-4 p-2" data-aos="fade-right">
+        Contact Me
+      </h1>
       <Row className="justify-content-center" data-aos="fade-up">
         <Col md={6}>
           <ListGroup className="m-5 p-4">
@@ -60,42 +86,57 @@ const Contact = () => {
           </ListGroup>
         </Col>
         <Col md={6}>
-          <Form className="m-5 p-4" id="contact">
-            <Form.Group className="mb-3" controlId="formBasicName">
-              <Form.Control
-                type="text"
-                placeholder="Your Name"
-                className="input-line"
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Control
-                type="email"
-                placeholder="Your Email Address"
-                className="input-line"
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicMessage">
-              <Form.Control
-                as="textarea"
-                rows={4}
-                placeholder="Your Message"
-                className="input-line"
-              />
-            </Form.Group>
-
-            <button
-              variant="primary"
-              className="mb-4"
-              class="btn btn-outline-dark"
-              size="lg"
-              type="submit"
-            >
-              Submit
-            </button>
-          </Form>
+          <div className="contact-form">
+            <Form ref={form} onSubmit={sendEmail} className="m-5 p-4">
+              <Row>
+                <Col md={6}>
+                  <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Control
+                      type="text"
+                      placeholder="Your Name"
+                      className="input-line"
+                      name="name"
+                    />
+                  </Form.Group>
+                </Col>
+                <Col md={6}>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control
+                      type="email"
+                      placeholder="Your Email Address"
+                      className="input-line"
+                      name="email"
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Form.Group className="mb-3" controlId="formBasicSubject">
+                <Form.Control
+                  type="text"
+                  placeholder="Subject"
+                  className="input-line"
+                  name="subject"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicMessage">
+                <Form.Control
+                  as="textarea"
+                  rows={4}
+                  placeholder="Your Message"
+                  className="input-line"
+                  name="message"
+                />
+              </Form.Group>
+              <button
+                variant="primary"
+                className="mb-4 btn btn-outline-dark"
+                size="lg"
+                type="submit"
+              >
+                Submit
+              </button>
+            </Form>
+          </div>
         </Col>
       </Row>
       <Map />
